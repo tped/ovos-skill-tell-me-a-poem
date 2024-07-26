5b72eeb683dbc7ad1069562784c9983a8daab69f
tped <tom@tpedersen.net>
2024-07-26 08:10:12 -0500

    Sample XML poem ... not currently used

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ from setuptools import setup
 from os import walk, path
 
 BASEDIR = path.abspath(path.dirname(__file__))
-URL = "TODO: Add 'repositoryUrl' to .projenrc.json and run pj"
-SKILL_CLAZZ = "TODO: Add 'skillClass' to .projenrc.json and run pj"  # needs to match __init__.py class name
+URL = "https://github.com/tped/ovos-skill-tell-me-a-poem"
+SKILL_CLAZZ = "TellMeAPoemSkill"  # needs to match __init__.py class name
 PYPI_NAME = "ovos-skill-tell-me-a-poem"  # pip install PYPI_NAME
 
 # below derived from github url to ensure standard skill_id
@@ -69,7 +69,7 @@ setup(
     long_description_content_type="text/markdown",
     url=URL,
     author="TODO: Add 'author' to .projenrc.json and run pj",
-    author_email="TODO: Add 'authorAddress' to .projenrc.json and run pj",
+    author_email="tom@tpedersen.net",
     license="# TODO: Add 'license' to .projenrc.json and run pj",
     package_dir={SKILL_PKG: "src"},
     package_data={SKILL_PKG: find_resource_files()},
--- src/__init__.py
+++ src/__init__.py
@@ -1,3 +1,5 @@
+# from typing import List, Dict, Any
+
 from ovos_utils import classproperty
 from ovos_utils.process_utils import RuntimeRequirements
 from ovos_workshop.intents import IntentBuilder
@@ -5,14 +7,20 @@ from ovos_workshop.decorators import intent_handler
 # from ovos_workshop.intents import IntentHandler # Uncomment to use Adapt intents
 from ovos_workshop.skills import OVOSSkill
 
+import json
+import os
+import random
+# import sys
+
 # Optional - if you want to populate settings.json with default values, do so here
 DEFAULT_SETTINGS = {
-    "setting1": True,
-    "setting2": 50,
-    "setting3": "test"
+    "PoetrySetting1": True,
+    "PoetrySetting2": 50,
+    "PoetryFilename": "src/data/poems/BrautiganPoems.json"
 }
 
-class HelloWorldSkill(OVOSSkill):
+
+class TellMeAPoemSkill(OVOSSkill):
     def __init__(self, *args, bus=None, **kwargs):
         """The __init__ method is called when the Skill is first constructed.
         Note that self.bus, self.skill_id, self.settings, and
@@ -23,6 +31,50 @@ class HelloWorldSkill(OVOSSkill):
         """
         super().__init__(*args, bus=bus, **kwargs)
         self.learning = True
+        self.poems = []
+        poem_file = self.settings.get('PoetryFilename')
+        self.load_poems(poem_file)  # Update the path to your JSON file
+
+    def load_poems(self, filepath):
+        if not os.path.isfile(filepath):
+            self.log.error(f"The file {filepath} does not exist.")
+            return
+
+        try:
+            with open(filepath, 'r') as file:
+                data = json.load(file)
+
+            if 'DOC' in data:
+                book_title = data['DOC']['TITLE'][0]
+                book_author = data['DOC']['AUTHOR'][0]
+
+                if 'SECTION' in data['DOC']:
+                    for section in data['DOC']['SECTION']:
+                        section_title = section['SECTIONTITLE'][0] if 'SECTIONTITLE' in section else None
+
+                        for poem in section['POEMS']:
+                            self.poems.append({
+                                "book_title": book_title,
+                                "book_author": book_author,
+                                "section_title": section_title,
+                                "poem_title": poem['TITLE'][0] if 'TITLE' in poem else "Unknown Title",
+                                "poem_author": poem['AUTHOR'][0] if 'AUTHOR' in poem else book_author,
+                                "content": poem['CONTENT'][0]
+                            })
+                else:
+                    for poem in data['DOC']['POEMS']:
+                        self.poems.append({
+                            "book_title": book_title,
+                            "book_author": book_author,
+                            "section_title": None,
+                            "poem_title": poem['TITLE'][0] if 'TITLE' in poem else "Unknown Title",
+                            "poem_author": poem['AUTHOR'][0] if 'AUTHOR' in poem else book_author,
+                            "content": poem['CONTENT'][0]
+                        })
+            else:
+                self.log.error("Invalid JSON structure: missing 'DOC'")
+        except Exception as e:
+            self.log.error(f"Error loading poems: {e}")
 
     def initialize(self):
         # merge default settings
@@ -51,11 +103,16 @@ class HelloWorldSkill(OVOSSkill):
         """
         return self.settings.get("my_setting", "default_value")
 
-    @intent_handler("HowAreYou.intent")
-    def handle_how_are_you_intent(self, message):
+    @intent_handler("TellMeAPoem.intent")
+    def handle_tell_me_a_poem_intent(self, message):
         """This is a Padatious intent handler.
         It is triggered using a list of sample phrases."""
-        self.speak_dialog("hello_world")
+        poem = random.choice(self.poems)
+        self.speak("Here's one of my favorites from the book " + str({poem["book_title"]}))
+        self.speak("by " + str({poem["book_author"]}))
+        self.speak("the poem is called " + str({poem["poem_title"]}))
+        self.speak("and it goes like this ")
+        self.speak(str({poem['content']}))
 
     @intent_handler(IntentBuilder("HelloWorldIntent").require("HelloWorldKeyword"))
     def handle_hello_world_intent(self, message):
